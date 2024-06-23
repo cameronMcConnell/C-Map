98707364554023fe862f66e88df359cfc1f5a50f
Cameron McConnell <cameron@angry-dogman.com>
2024-06-23 15:50:47 -0500

    Added definitions for newIntMap and newStringMap.

--- src/c-map.c
+++ src/c-map.c
@@ -6,9 +6,40 @@ StringMap *newStringMap(size_t size, float loadFactor) {
         return NULL;
     }
 
-    
+    map->size = size;
+    map->loadFactor = loadFactor;
+    map->count = 0;
+    map->entries = (StringMapEntry*) malloc(map->size * sizeof(StringMapEntry));
+    if (map->entries == NULL) {
+        free(map);
+        return NULL;
+    }
+
+    for (size_t i = 0; i < map->size; i++) {
+        map->entries[i].isOccupied = 0;
+    }
+
+    return map;
 }
 
 IntMap *newIntMap(size_t size, float loadFactor) {
+    IntMap *map = (IntMap*) malloc(sizeof(IntMap));
+    if (map == NULL) {
+        return NULL;
+    }
 
+    map->size = size;
+    map->loadFactor = loadFactor;
+    map->count = 0;
+    map->entries = (IntMapEntry*) malloc(map->size * sizeof(IntMapEntry));
+    if (map->entries == NULL) {
+        free(map);
+        return NULL;
+    }
+
+    for (size_t i = 0; i < map->size; i++) {
+        map->entries[i].isOccupied = 0;
+    }
+
+    return map;
 }
